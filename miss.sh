77fe95dc6e84578ccf63d6a4afc2c3e318c22888
shaoying <shylinux@icloud.com>
2020-05-11 08:38:10 +0800

    opt some

--- miss.sh
+++ miss.sh
@@ -69,6 +69,7 @@ require miss.sh
 
 ish_miss_prepare_compile
 ish_miss_prepare_install
+ish_miss_prepare_session ${PWD##*/}
 
 # ish_miss_prepare_volcanos
 # ish_miss_prepare_icebergs
@@ -151,7 +152,8 @@ END
 ~ssh
 
 ~web
-    config serve meta.volcanos.path usr/volcanos
+    config serve meta.title ${PWD##*/}
+    config serve meta.legal.0 hi
 
 END
 
@@ -164,7 +166,7 @@ ish_miss_prepare_session() {
         tmux split-window -d -p 30 -t $name
         tmux split-window -d -h -t ${name}:shy.2
         tmux send-key -t ${name}:shy.2 ish_miss_log Enter
-        tmux send-key -t ${name}:shy.3 ish_miss_serve Enter
+        tmux send-key -t ${name}:shy.3 ish_miss_start Enter
     fi
 
     [ "$TMUX" = "" ] && tmux attach -t $name
@@ -176,11 +178,32 @@ ish_miss_prepare_volcanos() {
     ish_miss_create_link usr/volcanos $(require_path shylinux/volcanos)
 
     ish_miss_create_file $ish_miss_order_js <<END
-Volcanos("onengine", {
-    remote: function(event, can, msg, pane, cmds, cb) {
-        return false
-    }
-}, [], function(can) {})
+Volcanos("onengine", { river: {
+    "one": {name: "第一组", storm: {
+        "hello": {name: "应用1", action: [
+            {name: "some", help: "some", inputs: [
+                {type: "text", name: "one"},
+                {type: "button", name: "one"},
+            ], engine: function(event, can, msg, pane, cmds, cb) {
+                msg.Echo("hello world")
+                typeof cb == "function" && cb(msg)
+            }},
+        ]},
+        "world": {name: "应用2", action: [
+            {name: "hello", help: "world", inputs: [
+                {type: "text", name: "one", value: "pwd"},
+                {type: "button", name: "one"},
+            ], group: "cli", index: "system"},
+        ]},
+        "word": {name: "word", action: [
+            {name: "trans", help: "词汇", inputs: [
+                {type: "text", name: "word", value: "miss"},
+                {type: "text", name: "method", value: ""},
+                {type: "button", name: "翻译"},
+            ], group: "web.wiki.alpha", index: "trans"},
+        ]},
+    }},
+}, })
 END
 }
 ish_miss_prepare_icebergs() {
@@ -200,6 +223,7 @@ ish_miss_serve() {
 }
 ish_miss_start() {
     while true; do
+        echo $PATH
         date && ice.bin $@ 2>$ctx_log && echo -e "\n\nrestarting..." || break
     done
 }
