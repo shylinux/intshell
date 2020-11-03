35ee11bac183867f9d7460f241678b802529a35e
shaoying <shaoying@zuoyebang.com>
2020-11-03 16:40:06 +0800

    opt vim

--- base/web/dev.sh
+++ base/web/dev.sh
@@ -4,15 +4,18 @@ ish_ctx_dev_sid=${ish_ctx_dev_sid:=""}
 ish_ctx_dev_sid(){ echo $ish_ctx_dev_sid; }
 ish_ctx_dev_request() {
     local url=$ctx_dev/code/zsh/$1 && shift
-    ish_web_request "$url" "$@" pwd "${PWD}" pid "$$" sid "$ish_ctx_dev_sid" SHELL "$SHELL"
+    ish_web_request "$url" "$@" pwd "${PWD}" sid "$ish_ctx_dev_sid"
 }
 ish_ctx_dev_login() {
-    ish_ctx_dev_sid=$(ish_ctx_dev_request sess sid "$ish_ctx_dev_sid" username "$(whoami)" hostname "$(hostname)" pid "$$" pwd "$(pwd)")
+    ish_ctx_dev_sid=$(ish_ctx_dev_request sess username "$(whoami)" hostname "$(hostname)" pid "$$")
 }
 ish_ctx_dev_logout() {
     ish_ctx_dev_request sess cmds logout 2>/dev/null && ish_ctx_dev_sid=""
 }
 
+ish_ctx_dev_qrcode() {
+    ish_ctx_dev_request qrcode text "$@"
+}
 ish_ctx_dev_favor() {
     if [ -z "$1" ]; then
         ish_ctx_dev_request favor
@@ -22,11 +25,10 @@ ish_ctx_dev_favor() {
 }
 ish_ctx_dev_trash=${ish_ctx_dev_trash:=~/trash}
 ish_ctx_dev_trash() {
-    local from=$PWD/$1 to=$ish_ctx_dev_trash/`date +"%Y%m%d"`-`echo $PWD/$1| tr '/' '_'`
-    local size=`du -sb $from|cut -f1`
+    local size=`du -sb $from|cut -f1 2>/dev/null`
+    local from=$PWD/$1 to=$ish_ctx_dev_trash/`date +"%Y%m%d-%H%M%S"`-`echo $PWD/$1| tr '/' '_'`
     ish_ctx_dev_request trash hostname "$(hostname)" size "$size" from "$from" to "$to" &>/dev/null
-    mkdir -p $ish_ctx_dev_trash
-    mv $from $to
+    mkdir -p $ish_ctx_dev_trash &>/dev/null; mv $from $to
 }
 
 zshaddhistory() {
@@ -36,7 +38,7 @@ zshaddhistory() {
 }
 ish_ctx_dev_sync() {
     local cmd=`HISTTIMEFORMAT="%F %T " history|tail -n1`
-    [ -n "$cmd" ] && [ "$ish_ctx_dev_sync_last" != "$cmd" ] && ish_ctx_dev_request sync cmds history arg "$cmd" >/dev/null
+    [ -n "$cmd" ] && [ "$ish_ctx_dev_sync_last" != "$cmd" ] && ish_ctx_dev_request sync SHELL "$SHELL" cmds history arg "$cmd" >/dev/null
     ish_ctx_dev_sync_last=$cmd
 }
 ish_ctx_dev_init() {
@@ -54,18 +56,10 @@ ish_ctx_dev_exit() {
     ish_ctx_dev_logout
 }
 ish_ctx_dev_init
-return
 
-ish_ctx_dev_upload_help() {
-    ish_help_show usage "ish_ctx_dev_upload file"
-}
 ish_ctx_dev_upload() {
-    [ "$1" = "" ] && ish_ctx_dev_upload_help && return
     local file=$1 && shift
     ish_ctx_dev_request upload upload "@$file" $@
-}
-ish_ctx_dev_download_help() {
-    ish_help_show usage "ish_ctx_dev_download file"
 }
 ish_ctx_dev_download() {
     [ "$#" = "0" ] && ish_ctx_dev_request download && return
---
+++ misc/vim/go.vim
@@ -0,0 +1,13 @@
+syntax match Comment "#.*$"
+syntax match Comment "\<Name: [^,]*"
+syntax match Comment "\<Name: \"[^\"]*\""
+syntax match Comment "\<Help: \"[^\"]*\""
+
+highlight kitConst    ctermfg=yellow
+syntax match kitConst "\<kit\.[a-z0-9A-Z_.]*"
+
+highlight msgConst    ctermfg=cyan
+syntax match msgConst "\<ice\.[a-z0-9A-Z_.]*"
+syntax match msgConst "\<msg\.[a-z0-9A-Z_.]*"
+syntax match msgConst "\<m\.[a-z0-9A-Z_.]*"
+
