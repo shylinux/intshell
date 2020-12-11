b3380c94b4a42ba6536dc99fa15db16a124a4fb7
shylinux <shylinux@icloud.com>
2020-12-11 10:48:10 +0800

    opt some

--- misc/git/git.sh
+++ misc/git/git.sh
@@ -4,4 +4,6 @@ ish_ctx_dev_git_prepare() {
     git config --global alias.s status
     git config --global alias.b branch
     git config --global credential.helper store
+    git config --global push.default simple
+    git config --global color.ui always
 }
