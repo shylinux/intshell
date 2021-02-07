1e3f633302958f51baca43297f00b7da1cb6a124
shylinux <shylinux@icloud.com>
2021-02-07 16:15:55 +0800

    opt mp

---
+++ misc/vim/css.vim
@@ -0,0 +1,4 @@
+highlight cssKey    ctermfg=yellow
+
+syntax match ishKey "page"
+syntax match ishKey "view"
--- misc/vim/vim.sh
+++ misc/vim/vim.sh
@@ -19,10 +19,11 @@ ish_ctx_dev_vim_prepare() {
     ish_miss_create_link ~/.vim/autoload/auto.vim $from/auto.vim
 
     mkdir -p ~/.vim/syntax
-    ish_miss_create_link ~/.vim/syntax/javascript.vim $from/javascript.vim
     ish_miss_create_link ~/.vim/syntax/go.vim $from/go.vim
-    ish_miss_create_link ~/.vim/syntax/shy.vim $from/shy.vim
     ish_miss_create_link ~/.vim/syntax/sh.vim $from/sh.vim
+    ish_miss_create_link ~/.vim/syntax/shy.vim $from/shy.vim
+    ish_miss_create_link ~/.vim/syntax/javascript.vim $from/javascript.vim
+    ish_miss_create_link ~/.vim/syntax/css.vim $from/css.vim
 
     ish_miss_create_link ~/.vimrc $from/vimrc
     vim -c PlugInstall -c exit -c exit
