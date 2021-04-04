4a5a2e4ffbfb113f37370be9740a0ebc326d69c0
harveyshao <harveyshao@tencent.com>
2021-04-05 03:49:25 +0800

    opt some

--- misc/git/git.sh
+++ misc/git/git.sh
@@ -4,6 +4,7 @@ ish_ctx_dev_git_prepare() {
     git config --global alias.s status
     git config --global alias.b branch
     git config --global color.ui always
+    git config --global core.quotepath false
     git config --global credential.helper store
     git config --global push.default simple
     git config --global pull.ff only
