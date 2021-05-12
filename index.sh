c7ed033d7a291e2c209decf1941ad9e7d4bacfd8
harveyshao <harveyshao@tencent.com>
2021-05-12 11:13:21 +0800

    opt index

--- index.sh
+++ index.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-temp_intshell() { # 加载脚本 file...
-    for script in "$@"; do temp_source intshell/$script; done 
+export ctx_dev=${ctx_dev:="https://shylinux.com"}
+
+down_file() { # 下载文件 dir url
+    curl --create-dirs -o $1 -fsSL $ctx_dev/$2
 }
-temp_source() { # 加载文件 url arg...
-    ctx_temp=$(mktemp) && down_source $ctx_temp $1 && shift && source $ctx_temp "$@"
-}
-down_source() { # 下载文件 dir url
-    curl -fsSL $ctx_dev/$2 --create-dirs -o $1
+temp_file() { # 加载文件 url arg...
+    ctx_temp=$(mktemp) && down_file $ctx_temp $1 && shift && source $ctx_temp "$@"
 }
 
 prepare_ice() {
@@ -22,52 +21,46 @@ prepare_ice() {
         arm*) bin=${bin}.arm ;;
         *) bin=${bin}.386 ;;
     esac
-    down_source bin/ice.bin publish/$bin && chmod u+x bin/ice.bin
-    down_source bin/ice.sh publish/ice.sh && chmod u+x bin/ice.sh
+    down_file bin/ice.bin publish/$bin && chmod u+x bin/ice.bin
+    down_file bin/ice.sh publish/ice.sh && chmod u+x bin/ice.sh
 }
 prepare_tmux() {
-    down_source etc/tmux.conf intshell/misc/tmux/tmux.conf
-    down_source bin/tmux.sh intshell/misc/tmux/local.sh
+    down_file etc/tmux.conf intshell/misc/tmux/tmux.conf
+    down_file bin/tmux.sh intshell/misc/tmux/local.sh
+}
+prepare_script() {
+    for script in "$@"; do temp_file intshell/$script; done 
 }
 
-prepare_main() {
-    ctx_dev=${ctx_dev:="https://shylinux.com"}; case "$1" in
+main() {
+    case "$1" in
         source) # 源码安装
             git clone https://github.com/shylinux/contexts
             cd contexts && source etc/miss.sh
-            # ish_miss_serve
             ;;
         binary) # 应用安装
             export PATH=${PWD}/bin:$PATH ctx_log=${ctx_log:=/dev/stdout}
             shift && prepare_ice && bin/ice.sh serve serve start dev dev "$@"
             ;;
         dev) # 开发环境
-            temp_intshell plug.sh conf.sh miss.sh
+            prepare_script plug.sh conf.sh miss.sh
             case "$(uname)" in
-                Darwin)
-                    xcode-select --install 2>/dev/null
-                    # /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-                    ;;
-                Linux)
-                    # ish_log_request "mirrors.aliyun.com"
-                    # curl -o /etc/yum.repos.d/CentOS-Base.repo https://mirrors.aliyun.com/repo/Centos-8.repo && yum -y update
-                    yum install -y wget tmux make git vim
-                    ;;
+                Darwin) xcode-select --install 2>/dev/null ;;
+                Linux) yum install -y wget tmux make git vim ;;
             esac
 
-            # export ISH_CONF_HUB_PROXY=$ctx_dev/code/git/
-            git config --gloal url."https://shylinux.com/code/git/repos".insteadOf "https://github.com/shylinux"
-            down_source go.mod publish/go.mod && down_source etc/miss.sh publish/miss.sh && source etc/miss.sh
+            git config --gloal url."$ctx_dev/code/git/repos".insteadOf "https://github.com/shylinux"
+            down_file go.mod publish/go.mod && down_file etc/miss.sh publish/miss.sh && source etc/miss.sh
             ;;
-        ice) # 生产环境
+        app) # 生产环境
             export PATH=${PWD}/bin:$PATH ctx_log=${ctx_log:=/dev/stdout}
             shift && prepare_ice && bin/ice.sh serve serve start dev dev "$@"
             ;;
         *) # 终端环境
-            temp_intshell plug.sh conf.sh
-            temp_source publish/order.sh "$@"
+            prepare_script plug.sh conf.sh
+            temp_file publish/order.sh "$@"
             ;;
     esac
 }
 
-prepare_main "$@"
+main "$@"
