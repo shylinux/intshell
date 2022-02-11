f69cf34a929d68eecc0552155d711f79202cc714
shy <shylinux@163.com>
2022-02-11 09:30:06 +0800

    opt some

--- index.sh
+++ index.sh
@@ -63,6 +63,7 @@ prepare_ice() {
         arm*) bin=${bin}.arm ;;
         *) bin=${bin}.386 ;;
     esac
+		echo $bin
     _down_big_file bin/ice.bin publish/$bin && chmod u+x bin/ice.bin
     _down_file bin/ice.sh publish/ice.sh && chmod u+x bin/ice.sh
     [ -f bin/ice.bin ]
