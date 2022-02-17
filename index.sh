48e32589e2a99a8f89c0249d4cc2fe54d39e022b
harveyshao <harveyshao@tencent.com>
2022-02-17 11:26:42 +0800

    opt code

--- index.sh
+++ index.sh
@@ -4,29 +4,42 @@ export ctx_dev=${ctx_dev:="https://shylinux.com"}
 
 _down_big_file() { # 下载文件 dir url
 	[ -f "$1" ] && return || echo "download $ctx_dev/$2"
-	mkdir -p ${1%*/*}; if curl -h &>/dev/null; then
+	echo $1| grep "/" &>/dev/null && mkdir -p ${1%*/*}; if curl -h &>/dev/null; then
 		curl -o $1 -fL $ctx_dev/$2
 	else
 		wget -O $1 $ctx_dev/$2
 	fi
 }
+_down_files() { # 下载文件 file...
+    for file in "$@"; do _down_file $file publish/${file##*/}; done
+}
 _down_file() { # 下载文件 dir url
-    curl --create-dirs -o $1 -fsSL $ctx_dev/$2
+	echo $1| grep "/" &>/dev/null && mkdir -p ${1%*/*}; if curl -h &>/dev/null; then
+		curl -o $1 -fsSL $ctx_dev/$2
+	else
+		wget -O $1 $ctx_dev/$2
+	fi
 }
 _temp_file() { # 加载文件 url arg...
     ctx_temp=$(mktemp) && _down_file $ctx_temp $1 && shift && source $ctx_temp "$@"
 }
 _down_tars() { # 下载文件 file...
-    for file in "$@"; do
-        _down_tar $file publish/$file
-    done
+    for file in "$@"; do _down_tar $file publish/$file; done
 }
 _down_tar() { # 下载文件 file path
-    [ -f $1 ] && return
-    echo "download $ctx_dev/$2"
-    curl --create-dirs -o $1 -fL $ctx_dev/$2 && tar -xf $1
+    [ -f $1 ] && return; _down_big_file "$@" && tar -xf $1
 }
 
+prepare_script() {
+    for script in "$@"; do _temp_file intshell/$script; done 
+}
+prepare_package() {
+    _down_tars contexts.bin.tar.gz 
+    _down_file etc/path publish/path
+    local back=$PWD; cd ~/; _down_tars vim.tar.gz; cd $back
+    export VIM=$PWD/usr/install/vim-vim-12be734/_install/share/vim/vim82/
+    export LD_LIBRARY_PATH=$PWD/usr/local/lib
+}
 prepare_system() {
     case "$(uname)" in
         Darwin) xcode-select --install 2>/dev/null ;;
@@ -38,17 +51,6 @@ prepare_system() {
             fi
             ;;
     esac
-}
-prepare_package() {
-    _down_tars contexts.bin.tar.gz 
-    _down_tars contexts.lib.tar.gz 
-    _down_file etc/path publish/path
-    local back=$PWD; cd ~/; _down_tars vim.tar.gz; cd $back
-    export VIM=$PWD/usr/install/vim-vim-12be734/_install/share/vim/vim82/
-    export LD_LIBRARY_PATH=$PWD/usr/local/lib
-}
-prepare_script() {
-    for script in "$@"; do _temp_file intshell/$script; done 
 }
 prepare_tmux() {
     _down_file etc/tmux.conf intshell/dev/tmux/tmux.conf
@@ -102,15 +104,11 @@ main() {
             git config --global init.templatedir $PWD/usr/install/git-2.31.1/_install/share/git-core/templates/
             git config --global url."$ctx_dev".insteadOf https://shylinux.com
             git config --global init.defaultBranch master
-            _down_file go.mod publish/go.mod
-            _down_file go.sum publish/go.sum
-            _down_file etc/miss.sh publish/miss.sh
-            source etc/miss.sh
+            _down_files etc/miss.sh Makefile go.mod go.sum && source etc/miss.sh
             ;;
         *) # 终端环境
             prepare_script plug.sh conf.sh miss.sh
-            ish_sys_dev_login
-            ish_sys_dev_run "$@"
+            ish_sys_dev_login && ish_sys_dev_run "$@"
             ;;
     esac
 }
