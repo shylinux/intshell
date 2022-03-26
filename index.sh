fc431c75e15f5dc56cab3c89350c61ecec1f4b74
shy <shylinux@163.com>
2022-03-26 23:07:40 +0800

    opt some

--- index.sh
+++ index.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-export ctx_dev=${ctx_dev:="https://contexts.com.cn"}
+# export ctx_dev=${ctx_dev:="https://contexts.com.cn"}
+export ctx_dev=${ctx_dev:="https://shylinux.com"}
 
 _down_big_file() { # 下载文件 dir url
 	[ -f "$1" ] && return || echo "download $ctx_dev/$2"
@@ -96,7 +97,8 @@ main() {
             shift && cd contexts && source etc/miss.sh "$@"
             ;;
         binary) # 应用安装
-            export ctx_log=${ctx_log:=/dev/stdout} ctx_dev="https://contexts.com.cn"
+            # export ctx_log=${ctx_log:=/dev/stdout} ctx_dev="https://contexts.com.cn"
+            export ctx_log=${ctx_log:=/dev/stdout} ctx_dev="https://shylinux.com"
             shift && prepare_ice && bin/ice.bin forever serve "$@"
             ;;
         app) # 生产环境
