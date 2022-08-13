752bf3427387bccdceff760288ac34203094717b
shylinux <shylinux@icloud.com>
2022-08-13 22:24:05 +0800

    opt some

--- dev/tmux/tmux.sh
+++ dev/tmux/tmux.sh
@@ -2,7 +2,12 @@
 
 ish_dev_tmux_prepare() {
     ish_sys_link_create ~/.tmux.conf $PWD/usr/intshell/dev/tmux/tmux.conf
-    ish_sys_link_create ~/.tmux_local $PWD/usr/intshell/dev/tmux/tmux_local.conf
+	if [ "`tmux -V`" = "tmux 1.8" ]; then
+		ish_sys_link_create ~/.tmux_local $PWD/usr/intshell/dev/tmux/tmux-1.8.conf
+	else
+		ish_sys_link_create ~/.tmux_local $PWD/usr/intshell/dev/tmux/tmux_local.conf
+	fi
+
 }
 ish_dev_tmux_session() {
     local tmux_cmd="tmux -S bin/tmux.socket -f etc/tmux.conf"
