9a799978b8ccf4501ffe60251322cb03735b1489
shylinux <shylinux@163.com>
2022-09-05 16:02:43 +0800

    opt some

--- miss.sh
+++ miss.sh
@@ -165,7 +165,7 @@ ish_miss_make() {
  	local binarys=bin/ice.bin; echo && date
 	[ -f src/version.go ] || echo "package main" > src/version.go
 	[ -f src/binpack.go ] || echo "package main" > src/binpack.go
-	go build -v -o ${binarys} src/main.go src/version.go src/binpack.go && ./${binarys} forever restart &>/dev/null
+	CGO_ENABLED=0 go build -v -o ${binarys} src/main.go src/version.go src/binpack.go && ./${binarys} forever restart &>/dev/null
 }
 ish_miss_start() {
 	$ctx_bin forever start "$@"
