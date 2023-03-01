9cfc4b85bba64cf3151049a71bd47a0dbccb6a98
1 <1>
2023-03-01 13:36:14 +0800

    opt some

--- dev/git/git.sh
+++ dev/git/git.sh
@@ -5,9 +5,11 @@ ish_dev_git_prepare() {
     git config --global alias.b branch
     git config --global alias.l "log --oneline --decorate"
     git config --global color.ui always
+    git config --global core.autocrlf false
     git config --global core.quotepath false
     git config --global init.defaultBranch master
     git config --global credential.helper store
     git config --global push.default simple
     git config --global pull.ff only
 }
+
