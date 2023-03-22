f65df7e060630d3c5810d3df307184807193069c
shylinux <shylinux@163.com>
2023-03-23 02:45:13 +0800

    opt some

--- dev/vim/vim.sh
+++ dev/vim/vim.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 ish_dev_vim_prepare() {
-    local from=$PWD/usr/intshell/dev/vim; ish_sys_link_create ~/.vimrc $from/vimrc
-    to=$HOME/.vim/autoload; ish_sys_link_create $to/plug.vim $from/plug.vim; ish_sys_link_create $to/auto.vim $from/auto.vim
+	local from=$PWD/usr/intshell/dev/vim; ish_sys_link_create ~/.vimrc $from/vimrc
+	to=$HOME/.vim/autoload; ish_sys_link_create $to/plug.vim $from/plug.vim; ish_sys_link_create $to/auto.vim $from/auto.vim
 	to=$HOME/.vim/syntax; for p in $from/*.vim; do ish_sys_link_create $to/${p##*/} $p; done
-    vim -c PlugInstall -c exit -c exit; vim -c GoInstallBinaries -c exit -c exit
+}
+ish_dev_vim_plug_prepare() {
+	vim -c PlugInstall -c exit -c exit; vim -c GoInstallBinaries -c exit -c exit
 }
