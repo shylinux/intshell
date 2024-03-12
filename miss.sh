7690c74efe2f35dd07b6bc72e8f7367584a268d7
shy <shy@shylinux.com>
2024-03-12 21:02:39 +0800

    add some

--- miss.sh
+++ miss.sh
@@ -88,10 +88,6 @@ END
 ish_miss_prepare_project() {
 	# etc/init.shy
 	ish_sys_file_create "etc/init.shy" <<END
-~web.code
-	binpack insert usr/icons/
-~web.code.git
-	config service cmd git
 ~ssh
 	source local.shy
 END
